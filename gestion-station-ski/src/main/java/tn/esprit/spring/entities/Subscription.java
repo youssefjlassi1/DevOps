f384eaf0335c6f67ec6362a7788c482d6922c2c9
yasminemseddi <80263842+yasminemseddi@users.noreply.github.com>
2024-10-10 18:47:58 +0100

    2eme commit

--- gestion-station-ski/src/main/java/tn/esprit/spring/entities/Subscription.java
+++ gestion-station-ski/src/main/java/tn/esprit/spring/entities/Subscription.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-
 import javax.persistence.*;
 
 import lombok.AccessLevel;
@@ -12,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
+
 @Getter
 @Setter
 @ToString
@@ -21,13 +21,17 @@ import lombok.experimental.FieldDefaults;
 @Entity
 public class Subscription implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	Long numSub;
-	LocalDate startDate;
-	LocalDate endDate;
-	Float price;
-//	@Enumerated(EnumType.STRING)
-	TypeSubscription typeSub;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    Long numSub;
+    LocalDate startDate;
+    LocalDate endDate;
+    Float price;
 
+    @Enumerated(EnumType.STRING)
+    TypeSubscription typeSub;
+}
+
+enum TypeSubscription {
+    ANNUAL, MONTHLY, SEMESTRIEL
 }
