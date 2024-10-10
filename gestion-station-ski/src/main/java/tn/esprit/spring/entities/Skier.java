1071c835166507407ebbda95c4be94d2131bddcf
yasminemseddi <80263842+yasminemseddi@users.noreply.github.com>
2024-10-10 20:06:49 +0100

    Update

--- gestion-station-ski/src/main/java/tn/esprit/spring/entities/Skier.java
+++ gestion-station-ski/src/main/java/tn/esprit/spring/entities/Skier.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.*;
@@ -22,50 +23,32 @@ import lombok.experimental.FieldDefaults;
 @Entity
 public class Skier implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    Long numSkier;
-    String firstName;
-    String lastName;
-    LocalDate dateOfBirth;
-    String city;
+      @Id
+      @GeneratedValue(strategy=GenerationType.IDENTITY)
+      Long numSkier;
+      String firstName;
+      String lastName;
+      LocalDate dateOfBirth;
+      String city;
 
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-    Subscription subscription;
+      @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+      Subscription subscription;
 
-    @JsonIgnore
-    @ManyToMany
-    @JoinTable(
-            name = "excursion",
-            joinColumns = @JoinColumn(name = "numSkier"),
-            inverseJoinColumns = @JoinColumn(name = "numPiste"))
-    private Set<Piste> pistes;
+      @JsonIgnore
+      @ManyToMany
+      @JoinTable(
+                  name = "excursion",
+                  joinColumns = @JoinColumn(name = "numSkier"),
+                  inverseJoinColumns = @JoinColumn(name = "numPiste"))
+      private Set<Piste> pistes;
 
-    @OneToMany(mappedBy = "skier")
-    Set<Registration> registrations;
-    // Dans Skier.java
-public Subscription getSubscription() {
-    return subscription;
-}
 
-public void setSubscription(Subscription subscription) {
-    this.subscription = subscription;
-}
+      @OneToMany(mappedBy = "skier")
+      Set<Registration> registrations;
 
-public Set<Registration> getRegistrations() {
-    return registrations;
-}
 
-public void setRegistrations(Set<Registration> registrations) {
-    this.registrations = registrations;
-}
 
-public Set<Piste> getPistes() {
-    return pistes;
-}
 
-public void setPistes(Set<Piste> pistes) {
-    this.pistes = pistes;
-}
+
 
 }
