d774f814b487366ec77867f864f4363627bcf0a3
youssefjlassi1 <youssef.jlassi@esprit.tn>
2024-10-25 16:55:07 +0200

    Suppression de l'ancien projet gestion-station-ski

--- gestion-station-ski/src/main/java/tn/esprit/spring/entities/Color.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.spring.entities;
-
-public enum Color {
-	GREEN, BLUE, RED, BLACK
-}
--- gestion-station-ski/src/main/java/tn/esprit/spring/repositories/IPisteRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.entities.Piste;
-
-public interface IPisteRepository extends JpaRepository<Piste, Long> {
-
-}
